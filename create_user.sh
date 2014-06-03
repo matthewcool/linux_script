ecaf20dffa8056e3a20933b926bbca19c876f257
Matthew Hong <matthew.hong@tw.fujitsu.com>
2014-06-03 11:18:02 +0800

    2014/6/3 - Beta release 	modified:   create_user.sh

--- create_user.sh
+++ create_user.sh
@@ -2,8 +2,26 @@
 
 source include/sh-lib
 
-# check to run the script as root
+#check to run the script as root
 if ! check_root; then
-    error_msg "Please run the script as root"
+    error_msg "Please run the script as root!"
 fi
 
+#check args and print usage
+if [ $# -lt 1 ]; then
+    echo "Usage: $0 [user_name]..."
+fi
+
+for user_name in $@
+do
+    grep "^$user_name:" /etc/passwd > /dev/null
+    if [ $? -eq 0 ]; then
+        echo "The user '$user_name' already exists!"
+        exit 1
+    else
+        #default password is "password" for users
+        passwd=$(perl -e 'print crypt("password", "password")')
+        useradd -m -p $passwd $user_name -s /bin/bash
+        [ $? -eq 0 ] && echo "The user '$user_name' has been added to system!" || echo "Failed to add the user '$user_name' to system!"
+    fi
+done
